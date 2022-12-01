aa8d17e1b635223d7086d7cb6e72674ea8d1d4a1
matheus <freitas1matheus@gmail.com>
2022-12-01 20:56:24 -0300

    mixin and hook

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -21,8 +21,10 @@
 import { useStore } from "@/store";
 import { defineComponent } from "vue";
 
-import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from '@/store/tipo-mutacoes'
+import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes'
 import { TipoNotificacao } from "@/interfaces/INotificacao";
+
+import useNotificador from '@/hooks/notificador'
 
 export default defineComponent({
   name: "Formulario",
@@ -52,19 +54,18 @@ export default defineComponent({
       } else {
         this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
       }
+
       this.nomeDoProjeto = "";
-      this.store.commit(NOTIFICAR, {
-        titulo: 'Novo projeto foi salvo',
-        texto: 'Prontinho ;) seu projeto já está disponível.',
-        tipo: TipoNotificacao.SUCESSO
-      })
+      this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso!')
       this.$router.push('/projetos')
-    },
+    }
   },
   setup () {
     const store = useStore()
+    const { notificar } = useNotificador()
     return {
-      store
+      store,
+      notificar
     }
   }
 });
